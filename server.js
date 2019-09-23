c87450b7d4b1523719d174523d8da42eab68aed1
Esteban Davila <davilaesteban98@gmail.com>
2019-09-23 18:16:42 -0500

    removed reference to database variable

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(cors());
 
 
 app.get('/', (req, res) => {
-    res.send(database.users);
+    res.send('Working PogU');
 })
 
 app.post('/signin', (req, res) => {signin.handleSignIn(req, res, db, bcrypt)});
